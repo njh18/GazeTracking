02b7155b18e82f0768880b76aa14be92407f153e
njh18 <ngjunhao1996@gmail.com>
2021-03-15 12:15:34 +0800

    Updated Video test with data frame storage

---
+++ untitled9.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 24 23:37:24 2021
+
+@author: Jun Hso
+"""
+
+counter=0
+end_point=10
+
+#while 
+while counter<end_point:
+    counter+=1
+    print("a")
--- video_test.py
+++ video_test.py
@@ -6,13 +6,14 @@ Created on Sun Jan  3 21:31:52 2021
 """
 import os
 import cv2
+import pandas as pd
 from gaze_tracking import GazeTracking
 import matplotlib.pyplot as plt
 import time
 
 from moviepy.editor import *
 from video_rotation import video_rotation
-
+from extractNames import extractNames
 
 # Change directory to datasets to get data
 os.chdir("C:\\Users\\Jun Hso\\Desktop")
@@ -22,6 +23,10 @@ current_directory = os.getcwd()
 ######################Change this when necessary #################################
 video_name = "JHprac_2021_03_12_13_11_26.mp4"
 new_video_name = video_name[:-4]+"_rotated"+".mp4"
+
+#Getting Videoinformation
+video_info = extractNames(video_name)
+print(video_info)
 
 # rotating the video
 video_rotation(video_name, current_directory)
@@ -43,6 +48,13 @@ pupil_coord_eye_only = []
 eye_onlys = []
 start_time = time.time()
 
+
+# Create new dataframe for output
+column_names = ["Timeframe","X-Coord (Both)","Y-Coord (Both)","X-Coord (Head)","Y-Coord (Head)","X-Coord (Eye)","Y-Coord (Eye)"]
+new_df = pd.DataFrame(columns = column_names)
+print(new_df)
+
+
 # Read until video is completed
 while(cap.isOpened()):
     # Capture frame-by-frame
@@ -56,23 +68,19 @@ while(cap.isOpened()):
         frame = gaze.annotated_frame()
         text = ""
 
-
         milliseconds = cap.get(cv2.CAP_PROP_POS_MSEC)
 
         seconds = milliseconds//1000
-        milliseconds = milliseconds%1000
-        minutes = 0
-        hours = 0
+        milliseconds = (milliseconds%1000)/1000
+        timeframe = seconds + milliseconds
+        
+        #forcombining with datetime
+        #tdelta = pd.to_timedelta(df["Miliseconds"], unit="ms")
+        
+        
         if seconds >= 60:
             minutes = seconds//60
             seconds = seconds % 60
-
-        if minutes >= 60:
-            hours = minutes//60
-            minutes = minutes % 60
-
-        print(int(hours), int(minutes), int(seconds), int(milliseconds))
-
 
         if gaze.is_blinking():
             text = "Blinking"
@@ -89,41 +97,39 @@ while(cap.isOpened()):
         left_pupil_eye_only = gaze.pupil_left_coords_eye_only()
 
         if left_pupil is None:
-            print("Cant find pupil")
-            pass
+            both_x = 0
+            both_y = 0
         else:
-            pupil_coord.append((left_pupil[0], left_pupil[1], now-start_time))
-            print(pupil_coord)
+            both_x = left_pupil[0]
+            both_y = left_pupil[1]
 
         if left_pupil_head_only is None:
+            head_x = 0
+            head_y = 0
             pass
         else:
-            pupil_coord_head_only.append(
-                (left_pupil_head_only[0], left_pupil_head_only[1], now-start_time))
+            head_x = left_pupil_head_only[0]
+            head_y = left_pupil_head_only[1]
 
         if left_pupil_eye_only is None:
-            pass
+            eye_x = 0
+            eye_y = 0
         else:
-            pupil_coord_eye_only.append(
-                (left_pupil_eye_only[0], left_pupil_eye_only[1], now-start_time))
+            eye_x = left_pupil_eye_only[0]
+            eye_y = left_pupil_eye_only[1]
 
-        cv2.putText(frame, text, (90, 60),
-                    cv2.FONT_HERSHEY_DUPLEX, 1.6, (147, 58, 31), 2)
-
-        cv2.putText(frame, text, (90, 60),
-                    cv2.FONT_HERSHEY_DUPLEX, 1.6, (147, 58, 31), 2)
-
-        left_pupil = gaze.pupil_left_coords()
-        right_pupil = gaze.pupil_right_coords()
+  
         cv2.putText(frame, "Left pupil:  " + str(left_pupil), (90, 130),
                     cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
-        cv2.putText(frame, "Right pupil: " + str(right_pupil), (90, 165),
-                    cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
-        cv2.putText(frame, "ratio: " + str(gaze.horizontal_ratio()),
-                    (90, 195), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
+
 
         cv2.imshow("Demo", frame)
 
+
+        #Input all the calcuated values into new dataframe
+        new_row = {"Timeframe":timeframe,"X-Coord (Both)":both_x,"Y-Coord (Both)":both_y,
+                   "X-Coord (Head)":head_x,"Y-Coord (Head)":head_y,"X-Coord (Eye)":eye_x,"Y-Coord (Eye)":eye_y}
+        new_df = new_df.append(new_row,ignore_index=True)
         # Press Q on keyboard to  exit
         if cv2.waitKey(25) & 0xFF == ord('q'):
             break
@@ -132,3 +138,5 @@ while(cap.isOpened()):
     else:
         break
 
+
+print(new_df)
